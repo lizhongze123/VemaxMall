0da800c5e2c4edbc94cdbc490a4391e25ba17014
lzz <793223793@qq.com>
2017-11-30 15:48:25 +0800

    新增欢迎界面，权限判断，导入glide

---
+++ app/src/main/java/cn/xm/vemaxmall/main/WelcomeActivity.java
@@ -0,0 +1,70 @@
+package cn.xm.vemaxmall.main;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+
+import cn.lzz.utils.AppPermission;
+import cn.lzz.utils.ToastUtils;
+import cn.xm.vemaxmall.R;
+import cn.xm.vemaxmall.base.BaseActivity;
+
+
+public class WelcomeActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionCheck();
+        } else {
+            startApp();
+        }
+    }
+
+    private void permissionCheck() {
+        boolean hasRequest = AppPermission.requestAllUnGrantedPermission(this);
+        if (!hasRequest) {
+            startApp();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case AppPermission.PERMISSION_REQUEST_CODE:
+                String toastTip = null;
+                for (int i = 0; i < permissions.length; i++) {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        toastTip = "";
+                    }
+                }
+                if (toastTip != null) {
+                    ToastUtils.toastInBottom(this, "部分权限被禁用");
+                    ToastUtils.toastInBottom(this, "可能会导致部分功能失效");
+                }
+                startApp();
+                break;
+                default:
+        }
+    }
+
+    private void startApp() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+}
