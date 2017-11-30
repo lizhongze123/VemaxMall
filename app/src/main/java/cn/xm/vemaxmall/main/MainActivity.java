fc56484c615dc0c63e27873e4aa3233ebb2fbdf9
lzz <793223793@qq.com>
2017-11-30 17:01:07 +0800

    加入滑动标题栏透明度改变

--- app/src/main/java/cn/xm/vemaxmall/main/MainActivity.java
+++ app/src/main/java/cn/xm/vemaxmall/main/MainActivity.java
@@ -1,9 +1,7 @@
 package cn.xm.vemaxmall.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import cn.lzz.utils.ToastUtils;
 import cn.xm.vemaxmall.R;
 import cn.xm.vemaxmall.base.BaseActivity;
 
--- app/src/main/java/cn/xm/vemaxmall/main/WelcomeActivity.java
+++ app/src/main/java/cn/xm/vemaxmall/main/WelcomeActivity.java
@@ -60,7 +60,7 @@ public class WelcomeActivity extends BaseActivity{
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                Intent intent = new Intent(WelcomeActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
