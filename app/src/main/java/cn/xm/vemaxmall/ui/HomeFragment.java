b1126d457ab44d29c493e1fb799893faae4b4499
lzz <793223793@qq.com>
2017-12-01 14:13:31 +0800

    首页fragment加入滑动时输入框透明度渐变

--- app/src/main/java/cn/xm/vemaxmall/ui/HomeFragment.java
+++ app/src/main/java/cn/xm/vemaxmall/ui/HomeFragment.java
@@ -1,11 +1,15 @@
 package cn.xm.vemaxmall.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.youth.banner.Banner;
 
@@ -15,13 +19,18 @@ import cn.lzz.utils.ResoureUtils;
 import cn.xm.vemaxmall.R;
 import cn.xm.vemaxmall.base.BaseFragment;
 import cn.xm.vemaxmall.utils.GlideImageLoader;
+import cn.xm.vemaxmall.view.ObservableScrollView;
 
 
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements ObservableScrollView.ScrollViewListener{
 
+    private ObservableScrollView scrollView;
+    private LinearLayout titleBar;
     private View rootView;
     private SwipeRefreshLayout srl;
     private Banner banner;
+    private TextView tvInput;
+    private int imageHeight;
 
     public static HomeFragment newInstance(){
         HomeFragment homeFragment = new HomeFragment();
@@ -37,7 +46,27 @@ public class HomeFragment extends BaseFragment {
     }
 
     private void initViews() {
-        srl = (SwipeRefreshLayout) rootView.findViewById(R.id.srl);
+        tvInput = rootView.findViewById(R.id.tv_input);
+        scrollView = rootView.findViewById(R.id.scrollView);
+        titleBar = rootView.findViewById(R.id.titleBar);
+        banner = rootView.findViewById(R.id.banner);
+        titleBar.setBackgroundColor(Color.argb(0, 0xfd, 0x91, 0x5b));
+        ViewTreeObserver vto = banner.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                titleBar.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+                imageHeight = banner.getHeight();
+
+                scrollView.setScrollViewListener(HomeFragment.this);
+            }
+        });
+
+
+        scrollView.setScrollViewListener(this);
+
+        srl = rootView.findViewById(R.id.srl);
         srl.setColorSchemeColors(ResoureUtils.getColor(getContext(), R.color.colorTheme));
         srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -69,5 +98,19 @@ public class HomeFragment extends BaseFragment {
     }
 
 
-
+    @Override
+    public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
+        if (y <= 0) {
+            //设置标题的背景颜色
+            tvInput.setAlpha(0.4f);
+        } else if (y > 0 && y <= imageHeight) {
+            //滑动距离小于banner图的高度时，设置背景和字体颜色颜色透明度渐变
+            float scale = (float) y / imageHeight;
+            float alpha = (1 * scale);
+            tvInput.setAlpha(alpha);
+        } else {
+            //滑动到banner下面设置普通颜色
+            tvInput.setAlpha(1);
+        }
+    }
 }
