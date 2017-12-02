3784a52c5f3cb32524788d4879ed70e0c5b598cc
lzz <793223793@qq.com>
2017-12-02 14:49:44 +0800

    查询fragment加入拼音排序列表

---
+++ ext_utils/src/main/java/cn/lzz/utils/ResoureUtils.java
@@ -0,0 +1,22 @@
+package cn.lzz.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
+
+public class ResoureUtils {
+
+    public static Resources getResoure(Context context) {
+        return context.getResources();
+    }
+
+    public static Drawable getDrawable(Context context, int resId){
+        return getResoure(context).getDrawable(resId);
+    }
+
+    public static int getColor(Context context, int resid) {
+        return getResoure(context).getColor(resid);
+    }
+
+}
